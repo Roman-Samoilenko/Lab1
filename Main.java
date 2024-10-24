1021c017c84efb88b354ff7352bccd14f45216cc
Roman-Samoilenko <155414732+Roman-Samoilenko@users.noreply.github.com>
2024-10-24 12:28:41 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 import java.io.PrintStream;
-import java.util.Arrays;
 
 public class Main {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
-    public static void main(String[] args) {
 
+    public static void main(String[] args)
+    {
         long n1 = in.nextLong();
         long n2 = in.nextLong();
         long n3 = in.nextLong();
         long n4 = in.nextLong();
 
-        Long[] m = {n1, n2, n3, n4};
-        Arrays.sort(m);
-
-        out.print(m[2]);
+        if ((n1 > n2 && n1 > n3 && n1 < n4) || (n1 > n4 && n1 > n3 && n1 < n2) || (n1 > n4 && n1 > n2 && n1 < n3)) {
+            out.println(n1);
+        } else if ((n2 > n1 && n2 > n3 && n2 < n4) || (n2 > n4 && n2 > n3 && n2 < n1) || (n2 > n4 && n2 > n1 && n2 < n3)) {
+            out.println(n2);
+        } else if ((n3 < n1 && n3 > n2 && n3 > n4) || (n3 < n4 && n3 > n2 && n3 > n1) || (n3 < n2 && n3 > n1 && n3 > n4)) {
+            out.println(n3);
+        } else{
+            out.println(n4);
+        }
     }
 }
